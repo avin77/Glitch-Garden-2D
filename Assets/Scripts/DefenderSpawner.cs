0c12e246dacce66ced447e355cac428c6ac173b3
avin77 <shobhit.7@gmail.com>
2021-06-11 13:32:23 +0530

    Commit 2d plant vs zombies kind of game

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+
+    Defender defender;
+    private void OnMouseDown() {
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked() {
+        Vector2 screenPosition = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        worldPosition = RoundVector2Float(worldPosition);
+        return worldPosition;
+    }
+
+    private void SpawnDefender(Vector2 roundWorldPos) {
+        Defender newDefender = Instantiate(defender, roundWorldPos, Quaternion.identity) as Defender;
+    }
+
+    public void setSelectedDefender(Defender defenderToSelect) {
+        defender = defenderToSelect;
+    }
+
+    private Vector2 RoundVector2Float(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame
+    [SerializeField] float speed=1f;
+    [SerializeField] float healthReducedBy = 50;
+    void Update()
+    {
+        transform.Translate(Vector2.right*speed*Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollision)
+    {
+        var health = otherCollision.GetComponent<Health>();
+        var attacker = otherCollision.GetComponent<Attacker>();
+        if (attacker && health) {
+            health.dealDamage(healthReducedBy);
+            Destroy(gameObject);
+        }   
+    }
+
+    
+}
